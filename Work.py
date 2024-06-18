a9efda3aafafd55ff900f4a435fdde9eb5bfabfd
JustinDeanS <143033461+JustinDeanS@users.noreply.github.com>
2024-06-18 16:41:15 -0400

    added work practice

---
+++ Work.py
@@ -0,0 +1,163 @@
+GoogleFlights.SearchResult(
+  destination_airports=['ZAD'],
+  destination_name='Zadar',
+  flights_shopping_url='https://www.google.com/travel/flights?tfs=CCcQAhopEgoyMDI0LTA1LTI0agwIAxIIL20vMDk1d19yDQgDEgkvbS8wMXFxdDgaKRIKMjAyNC0wNS0yN2oNCAMSCS9tLzAxcXF0OHIMCAMSCC9tLzA5NXdfUgNVU0R6bENqUklXVkJTWmkxTFYzZHZTRlZCUVVGdVJHZERSeTB0TFMwdExTMHRMWFY1WTNCbU5FRkJRVUZCUjFwQlFUVlJTR3RKV1VGQkVnWkdValkxTWpFYUNnaVFGaEFDR2dOVlUwUTRNM0NRRmc9PboBAggC&curr=USD',
+  multi_destination_shopping_result=None,
+  one_way=False,
+  origin_airports=['BUD', 'XXQ'],
+  origin_destination_specific_dates_result=None,
+  origin_name='Budapest',
+  shopping_result=GoogleFlights.ShoppingResult(
+      departure_date='May 24',
+      destination_airports=None,
+      destination_name=None,
+      flights=[
+          GoogleFlights.Flight(
+              destination='ZAD',
+              origin='BUD',
+              airline=['Ryanair'],
+              airline_logo=(
+                  'https://www.gstatic.com/flights/airline_logos/70px/FR.png'
+              ),
+              arrival_time='11:50 PM',
+              departure_time='10:40 PM',
+              duration='1 hr 10 min',
+              flight_number=['economy class FR6521'],
+              flight_url='https://www.google.com/travel/flights?tfs=CCcQAhpHEgZNYXkgMjQiIAoDQlVEEgoyMDI0LTA1LTI0GgNaQUQqAkZSMgQ2NTIxagwIAxIIL20vMDk1d19yDQgDEgkvbS8wMXFxdDgaRxIGTWF5IDI3IiAKA0JVRBIKMjAyNC0wNS0yNBoDWkFEKgJGUjIENjUyMWoNCAMSCS9tLzAxcXF0OHIMCAMSCC9tLzA5NXdfUgNVU0R6bENqUklXVkJTWmkxTFYzZHZTRlZCUVVGdVJHZERSeTB0TFMwdExTMHRMWFY1WTNCbU5FRkJRVUZCUjFwQlFUVlJTR3RKV1VGQkVnWkdValkxTWpFYUNnaVFGaEFDR2dOVlUwUTRNM0NRRmc9PQ&curr=USD',
+              layover=None,
+              price='$29',
+              stops=0,
+          ),
+          GoogleFlights.Flight(
+              destination='ZAD',
+              origin='BUD',
+              airline=['Austrian'],
+              airline_logo=(
+                  'https://www.gstatic.com/flights/airline_logos/70px/OS.png'
+              ),
+              arrival_time='12:55 PM',
+              departure_time='7:00 AM',
+              duration='5 hr 55 min',
+              flight_number=['economy class OS718, ', 'economy class OS753'],
+              flight_url='https://www.google.com/travel/flights?tfs=CCcQAhpnEgZNYXkgMjQiHwoDQlVEEgoyMDI0LTA1LTI0GgNWSUUqAk9TMgM3MTgiHwoDVklFEgoyMDI0LTA1LTI0GgNaQUQqAk9TMgM3NTNqDAgDEggvbS8wOTV3X3INCAMSCS9tLzAxcXF0OBpnEgZNYXkgMjciHwoDQlVEEgoyMDI0LTA1LTI0GgNWSUUqAk9TMgM3MTgiHwoDVklFEgoyMDI0LTA1LTI0GgNaQUQqAk9TMgM3NTNqDQgDEgkvbS8wMXFxdDhyDAgDEggvbS8wOTV3X1IDVVNEenRDalJJV1ZCU1ppMUxWM2R2U0ZWQlFVRnVSR2REUnkwdExTMHRMUzB0TFhWNVkzQm1ORUZCUVVGQlIxcEJRVFZSU0d0SldVRkJFZ3RQVXpjeE9IeFBVemMxTXhvTENNM0pBUkFDR2dOVlUwUTRNM0ROeVFFPQ&curr=USD',
+              layover=[
+                  GoogleFlights.Layover(
+                      duration='4 hr 5 min', location='Vienna (VIE)'
+                  )
+              ],
+              price='$259',
+              stops=1,
+          ),
+          GoogleFlights.Flight(
+              destination='ZAD',
+              origin='BUD',
+              airline=['Austrian', 'Croatia'],
+              airline_logo='https://www.gstatic.com/flights/airline_logos/70px/multi.png',
+              arrival_time='10:00 PM',
+              departure_time='11:20 AM',
+              duration='10 hr 40 min',
+              flight_number=[
+                  'economy class OS714, ',
+                  'economy class OS677, ',
+                  'economy class OU632',
+              ],
+              flight_url='https://www.google.com/travel/flights?tfs=CCcQAhqIARIGTWF5IDI0Ih8KA0JVRBIKMjAyNC0wNS0yNBoDVklFKgJPUzIDNzE0Ih8KA1ZJRRIKMjAyNC0wNS0yNBoDWkFHKgJPUzIDNjc3Ih8KA1pBRxIKMjAyNC0wNS0yNBoDWkFEKgJPVTIDNjMyagwIAxIIL20vMDk1d19yDQgDEgkvbS8wMXFxdDgaiAESBk1heSAyNyIfCgNCVUQSCjIwMjQtMDUtMjQaA1ZJRSoCT1MyAzcxNCIfCgNWSUUSCjIwMjQtMDUtMjQaA1pBRyoCT1MyAzY3NyIfCgNaQUcSCjIwMjQtMDUtMjQaA1pBRCoCT1UyAzYzMmoNCAMSCS9tLzAxcXF0OHIMCAMSCC9tLzA5NXdfUgNVU0R6fENqUklXVkJTWmkxTFYzZHZTRlZCUVVGdVJHZERSeTB0TFMwdExTMHRMWFY1WTNCbU5FRkJRVUZCUjFwQlFUVlJTR3RKV1VGQkVoRlBVemN4Tkh4UFV6WTNOM3hQVlRZek1ob0xDTk8vQWhBQ0dnTlZVMFE0TTNEVHZ3ST0&curr=USD',
+              layover=[
+                  GoogleFlights.Layover(
+                      duration='5 hr 10 min', location='Vienna (VIE)'
+                  ),
+                  GoogleFlights.Layover(
+                      duration='3 hr 10 min', location='Zagreb (ZAG)'
+                  ),
+              ],
+              price='$410',
+              stops=2,
+          ),
+          GoogleFlights.Flight(
+              destination='ZAD',
+              origin='BUD',
+              airline=['Austrian', 'Croatia'],
+              airline_logo='https://www.gstatic.com/flights/airline_logos/70px/multi.png',
+              arrival_time='10:00 PM',
+              departure_time='11:20 AM',
+              duration='10 hr 40 min',
+              flight_number=[
+                  'economy class OS714, ',
+                  'economy class OU443, ',
+                  'economy class OU632',
+              ],
+              flight_url='https://www.google.com/travel/flights?tfs=CCcQAhqIARIGTWF5IDI0Ih8KA0JVRBIKMjAyNC0wNS0yNBoDVklFKgJPUzIDNzE0Ih8KA1ZJRRIKMjAyNC0wNS0yNBoDWkFHKgJPVTIDNDQzIh8KA1pBRxIKMjAyNC0wNS0yNBoDWkFEKgJPVTIDNjMyagwIAxIIL20vMDk1d19yDQgDEgkvbS8wMXFxdDgaiAESBk1heSAyNyIfCgNCVUQSCjIwMjQtMDUtMjQaA1ZJRSoCT1MyAzcxNCIfCgNWSUUSCjIwMjQtMDUtMjQaA1pBRyoCT1UyAzQ0MyIfCgNaQUcSCjIwMjQtMDUtMjQaA1pBRCoCT1UyAzYzMmoNCAMSCS9tLzAxcXF0OHIMCAMSCC9tLzA5NXdfUgNVU0R6fENqUklXVkJTWmkxTFYzZHZTRlZCUVVGdVJHZERSeTB0TFMwdExTMHRMWFY1WTNCbU5FRkJRVUZCUjFwQlFUVlJTR3RKV1VGQkVoRlBVemN4Tkh4UFZUUTBNM3hQVlRZek1ob0xDTk8vQWhBQ0dnTlZVMFE0TTNEVHZ3ST0&curr=USD',
+              layover=[
+                  GoogleFlights.Layover(
+                      duration='7 hr 40 min', location='Vienna (VIE)'
+                  ),
+                  GoogleFlights.Layover(
+                      duration='40 min', location='Zagreb (ZAG)'
+                  ),
+              ],
+              price='$410',
+              stops=2,
+          ),
+          GoogleFlights.Flight(
+              destination='ZAD',
+              origin='BUD',
+              airline=['Discover Airlines', 'Lufthansa'],
+              airline_logo='https://www.gstatic.com/flights/airline_logos/70px/multi.png',
+              arrival_time='7:00 PM',
+              departure_time='11:10 AM',
+              duration='7 hr 50 min',
+              flight_number=['economy class LH1337, ', 'economy class 4Y910'],
+              flight_url='https://www.google.com/travel/flights?tfs=CCcQAhpoEgZNYXkgMjQiIAoDQlVEEgoyMDI0LTA1LTI0GgNGUkEqAkxIMgQxMzM3Ih8KA0ZSQRIKMjAyNC0wNS0yNBoDWkFEKgI0WTIDOTEwagwIAxIIL20vMDk1d19yDQgDEgkvbS8wMXFxdDgaaBIGTWF5IDI3IiAKA0JVRBIKMjAyNC0wNS0yNBoDRlJBKgJMSDIEMTMzNyIfCgNGUkESCjIwMjQtMDUtMjQaA1pBRCoCNFkyAzkxMGoNCAMSCS9tLzAxcXF0OHIMCAMSCC9tLzA5NXdfUgNVU0R6dENqUklXVkJTWmkxTFYzZHZTRlZCUVVGdVJHZERSeTB0TFMwdExTMHRMWFY1WTNCbU5FRkJRVUZCUjFwQlFUVlJTR3RKV1VGQkVneE1TREV6TXpkOE5GazVNVEFhQ3dpYXhBSVFBaG9EVlZORU9ETndtc1FD&curr=USD',
+              layover=[
+                  GoogleFlights.Layover(
+                      duration='4 hr 35 min', location='Frankfurt (FRA)'
+                  )
+              ],
+              price='$415',
+              stops=1,
+          ),
+      ],
+      flights_shopping_url=None,
+      origin_airports=None,
+      origin_name=None,
+      return_date='May 27',
+  ),
+)
+[
+  GoogleSearch.SearchResult(
+      snippet=(
+          'Budapest to Zadar from $31 (€27) - Travel - Omio\n\nThe cheapest'
+          ' way to go to Zadar from Budapest is by taking a bus, which costs'
+          ' on average $31 (€27). This is compared to other ways of getting'
+          ' from Budapest to Zadar: Taking a bus costs $332 (€290) less than'
+          ' taking a flight, which average ticket prices of $363 (€317).'
+      ),
+      source_title='',
+      url='https://www.omio.com/travel/budapest/zadar#:~:text=The%20cheapest%20way%20to%20go,of%20%24363%20(%E2%82%AC317).',
+  ),
+  GoogleSearch.SearchResult(
+      snippet=(
+          'Questions & Answers. What is the cheapest way to get from Budapest'
+          ' to Zadar? The cheapest way to get from Budapest to Zadar is to'
+          ' train and bus which costs 7 000 Ft - 11 000 Ft and takes 10h 41m.'
+          ' What is the fastest way to get from Budapest to Zadar? The'
+          ' fastest way to get from Budapest to Zadar is to fly which takes'
+          ' 5h 10m and costs 16 000 Ft - 110 000 Ft.\nBudapest to Zadar bus'
+          ' services, operated by FlixBus, depart from Budapest-Kelenföld'
+          ' station. Bus or fly from Budapest to Zadar? The best way to get'
+          ' from Budapest to Zadar is to bus which takes 7h 30m and costs 18'
+          ' 000 Ft - 26 000 Ft. Alternatively, you can fly, which costs 16'
+          ' 000 Ft - 110 000 Ft and takes 5h 10m. Where does the Budapest to'
+          ' Zadar bus arrive? Budapest to Zadar bus services, operated by'
+          ' FlixBus, arrive at Zadar bus station. Can I drive from Budapest'
+          ' to Zadar? Yes, the driving distance between Budapest to Zadar is'
+          ' 628 km. It takes approximately 6h 4m to drive from Budapest to'
+          ' Zadar.'
+      ),
+      source_title=(
+          'Budapest to Zadar - 9 ways to travel via train, bus, car, plane,'
+          ' and shuttle - Rome2Rio'
+      ),
+      url='https://www.rome2rio.com/s/Budapest/Zadar',
+  ),
+]
